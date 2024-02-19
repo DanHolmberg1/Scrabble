43a9a7ac1efb8de38d4fa7b6e0cb8e535bb8dffc
olpers <antonaxelhedlund@gmail.com>
2024-02-19 16:32:36 +0100

    players och scores

---
+++ lib/players.ts
@@ -0,0 +1,37 @@
+type PlayerNumber = 1 | 2;
+type User = {
+    userName: string
+    password: string
+    highScore: number
+}
+
+let player1 = {
+    currentScore: 0,
+    currentTiles: [],
+    user: ""
+}
+
+let player2 = {
+    currentScore: 0,
+    currentTiles: [],
+    user: ""
+}
+
+function getPlayerScore(player: PlayerNumber): number {
+    return player == 1 ? player1.currentScore : player2.currentScore 
+}
+
+function addPlayerScore(player: PlayerNumber, addScore: number): void {
+    player == 1 ? player1.currentScore = player1.currentScore + addScore :
+                  player2.currentScore = player2.currentScore + addScore;
+}
+
+function resetScores(): void {
+    player1.currentScore = 0;
+    player2.currentScore = 0;
+}
+
+function setUserName(player: PlayerNumber, userName: string): void {
+    player == 1 ? player1.user = userName : player2.user = userName;
+}
+
