4b49b7269f27da91410e3db03150308edc9308d0
DanHolmberg1 <dan.holmberg1@hotmail.com>
2024-02-19 12:49:55 +0000

    Fixed with webpack, can now import

--- main.ts
+++ main.ts
@@ -1,207 +1,8 @@
+import * as q from './lib/queue_array';
+import {generateRandomLetters} from './lib/randomLetters';
+
 type cell<A, B> = { row: A; col: A; special: A; char: B };
-//import * as q from './queue_array';
-//import {generateRandomLetters} from './randomLetters';
 
-//Import function but import not working
-
-/**
- * A homogeneous queue.
- * The first entry points to the index of the queue's head element,
- * the second entry points to the next empty index of the queue, and
- * the last entry holds the values (contents) of the queue.
- * @template T type of all queue elements
- */
-type Queue<T> = [number, number, Array<T>];
-
-/**
- * Constructs a queue without any elements.
- * @template T type of all queue elements
- * @returns Returns an empty queue.
- */
-function empty<T>(): Queue<T> {
-  return [0, 0, []];
-}
-
-/**
- * Checks whether a queue is empty.
- * @template T type of all queue elements
- * @param q queue to check for emptiness
- * @returns Returns true, if the queue q has elements, false otherwise.
- */
-function is_empty<T>(q: Queue<T>): boolean {
-  return q[0] === q[1];
-}
-
-/**
- * Adds an element to the queue.
- * @template T type of all queue elements
- * @param e element to add
- * @param q queue to modify
- * @modifies q by adding element e to the end
- */
-function enqueue<T>(e: T, q: Queue<T>) {
-  const tail_index = q[1];
-  q[2][tail_index] = e;
-  q[1] = tail_index + 1; // update tail index
-}
-
-/**
- * Retrieves the first element of the queue.
- * @precondition Assumes q to be non-empty
- * @template T type of all queue elements
- * @param q queue to get the first element of
- * @returns Returns the element of the queue that was enqueued first.
- */
-function head<T>(q: Queue<T>): T {
-  const head_index = q[0];
-  return q[2][head_index];
-}
-
-/**
- * Removes the first element of a queue.
- * @precondition Assumes q to be non-empty
- * @template T type of all queue elements
- * @param q queue to remove the element from
- * @modifies q such that the element that was enqueued first is removed
- */
-function dequeue<T>(q: Queue<T>) {
-  const head_index = q[0];
-  q[0] = head_index + 1;
-}
-
-/**
- * Pretty-prints the contents of a queue to standard output.
- * @template T type of all queue elements
- * @param q queue to pretty-print
- */
-function display_queue<T>(q: Queue<T>) {
-  console.log(q[2].slice(q[0], q[1]));
-}
-
-function generateRandomLetters(): Queue<string> {
-  const letters = [
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "A",
-    "B",
-    "B",
-    "C",
-    "D",
-    "D",
-    "D",
-    "D",
-    "D",
-    "E",
-    "E",
-    "E",
-    "E",
-    "E",
-    "E",
-    "E",
-    "F",
-    "F",
-    "G",
-    "G",
-    "G",
-    "H",
-    "H",
-    "I",
-    "I",
-    "I",
-    "I",
-    "I",
-    "J",
-    "K",
-    "K",
-    "K",
-    "L",
-    "L",
-    "L",
-    "L",
-    "L",
-    "M",
-    "M",
-    "M",
-    "N",
-    "N",
-    "N",
-    "N",
-    "N",
-    "O",
-    "O",
-    "O",
-    "O",
-    "O",
-    "P",
-    "P",
-    "R",
-    "R",
-    "R",
-    "R",
-    "R",
-    "R",
-    "R",
-    "R",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-    "S",
-    "T",
-    "T",
-    "T",
-    "T",
-    "T",
-    "T",
-    "T",
-    "T",
-    "U",
-    "U",
-    "U",
-    "V",
-    "V",
-    "X",
-    "Y",
-    "Z",
-    "Å",
-    "Å",
-    "Ä",
-    "Ä",
-    "Ö",
-    "Ö",
-  ];
-  let randomLetters: Queue<string> = empty();
-  let lettersScrambled = shuffle(letters);
-  for (let i = 0; i < lettersScrambled.length; i++) {
-    enqueue(lettersScrambled[i], randomLetters);
-  }
-  return randomLetters;
-}
-function shuffle(array: Array<string>) {
-  let currentIndex = array.length,
-    randomIndex;
-  // While there remain elements to shuffle.
-  while (currentIndex > 0) {
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex],
-      array[currentIndex],
-    ];
-  }
-  return array;
-}
-//
 
 let gameBoard: Array<Array<Array<cell<number, string>>>> = [];
 
@@ -249,7 +50,6 @@ function createBoard(
       if (speicalSquares[row][col] === 0) {
         boardElement.appendChild(cell);
         cell.setAttribute("id", id);
-        continue;
       } else if (speicalSquares[row][col] === 1) {
         cell.textContent = "DL";
         cell.setAttribute("data-special", "double-letter");
@@ -344,15 +144,15 @@ document.addEventListener("DOMContentLoaded", () => {
     createBoard(boardElement, 15, 15); // Your existing board creation logic
   }
 
-  let letterQueue: Queue<string> = generateRandomLetters();
+  let letterQueue: q.Queue<string> = generateRandomLetters();
   let leftLetters: string = "";
   let rightLetters: string = "";
 
   for (let i = 0; i < 7; i++) {
-    leftLetters +=head(letterQueue)
-    dequeue(letterQueue);
-    rightLetters += head(letterQueue);
-    dequeue(letterQueue);
+    leftLetters += q.head(letterQueue)
+    q.dequeue(letterQueue);
+    rightLetters += q.head(letterQueue);
+    q.dequeue(letterQueue);
   }
   createTilesForLetters("leftTiles", leftLetters);
   createTilesForLetters("rightTiles", rightLetters);
