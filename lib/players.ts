fbc7c3d04f93e0f2c89267feb6e783ac07a4810a
olpers <antonaxelhedlund@gmail.com>
2024-02-22 14:54:51 +0100

    exports i players

--- lib/players.ts
+++ lib/players.ts
@@ -25,36 +25,36 @@ let player2: Player = {
   currentWord: [],
 };
 
-function getPlayerScore(player: PlayerNumber): number {
+export function getPlayerScore(player: PlayerNumber): number {
   return player == 1 ? player1.currentScore : player2.currentScore;
 }
 
-function addPlayerScore(player: PlayerNumber, addScore: number): void {
+export function addPlayerScore(player: PlayerNumber, addScore: number): void {
   player == 1
     ? (player1.currentScore = player1.currentScore + addScore)
     : (player2.currentScore = player2.currentScore + addScore);
 }
 
-function resetScores(): void {
+export function resetScores(): void {
   player1.currentScore = 0;
   player2.currentScore = 0;
 }
 
-function setUserName(player: PlayerNumber, userName: string): void {
+export function setUserName(player: PlayerNumber, userName: string): void {
   player == 1 ? (player1.user = userName) : (player2.user = userName);
 }
 
-function resetCurrentWord(): void {
+export function resetCurrentWord(): void {
   player1.currentWord = [];
   player2.currentWord = [];
 }
 
-function addToCurrentWord(player: PlayerNumber, currentCell: cell<number, string>): void {
+export function addToCurrentWord(player: PlayerNumber, currentCell: cell<number, string>): void {
   player == 1 ? player1.currentWord.push(currentCell) 
               : player2.currentWord.push(currentCell);
 }
 
-function removeFromCurrentWord(player: PlayerNumber, currentCell: cell<number, string>): void {
+export function removeFromCurrentWord(player: PlayerNumber, currentCell: cell<number, string>): void {
   const playerWord: Array<cell<number, string>> = (player == 1 ? player1.currentWord 
                                                              : player2.currentWord);
   const index: number = playerWord.indexOf(currentCell);
@@ -65,7 +65,7 @@ function removeFromCurrentWord(player: PlayerNumber, currentCell: cell<number, s
 
 }
 
-function getCurrentWord(player: PlayerNumber): Array<cell<number, string>> {
+export function getCurrentWord(player: PlayerNumber): Array<cell<number, string>> {
   return player == 1 ? player1.currentWord : player2.currentWord;
 }
 
